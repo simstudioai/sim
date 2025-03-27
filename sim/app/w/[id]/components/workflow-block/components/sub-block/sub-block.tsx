713dfe10583ad5a789aef1258d13311e42f4e8ad
ntwari bruce <ntwaribruce647@gmail.com>
2025-03-27 17:20:51 -0500

    Agent chat history

--- sim/app/w/[id]/components/workflow-block/components/sub-block/sub-block.tsx
+++ sim/app/w/[id]/components/workflow-block/components/sub-block/sub-block.tsx
@@ -3,6 +3,7 @@ import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip
 import { useWorkflowStore } from '@/stores/workflows/workflow/store'
 import { getBlock } from '@/blocks/index'
 import { SubBlockConfig } from '@/blocks/types'
+import { ChatHistory } from './components/chat-history'
 import { CheckboxList } from './components/checkbox-list'
 import { Code } from './components/code'
 import { ConditionInput } from './components/condition-input'
@@ -45,6 +46,8 @@ export function SubBlock({ blockId, config, isConnecting }: SubBlockProps) {
 
   const renderInput = () => {
     switch (config.type) {
+      case 'chat-history':
+        return <ChatHistory blockId={blockId} subBlockId={config.id} />
       case 'short-input':
         return (
           <ShortInput
@@ -155,9 +158,9 @@ export function SubBlock({ blockId, config, isConnecting }: SubBlockProps) {
           />
         )
       case 'file-selector':
-        return <FileSelectorInput blockId={blockId} subBlock={config} disabled={isConnecting} />
+        return <FileSelectorInput blockId={blockId} subBlock={config} disabled={isConnecting} /> 
       case 'folder-selector':
-        return <FolderSelectorInput blockId={blockId} subBlock={config} disabled={isConnecting} />
+        return <FolderSelectorInput blockId={blockId} subBlock={config} disabled={isConnecting} /> 
       default:
         return null
     }
