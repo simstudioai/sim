00429bcea4a047d61d9be116ff8816b66a444b27
Adam Gough <adamgough@Adams-MacBook-Pro.local>
2025-04-28 17:27:48 -0700

    telegram message tool

---
+++ sim/blocks/blocks/telegram.ts
@@ -0,0 +1,55 @@
+import { TelegramIcon } from '@/components/icons'
+import { TelegramMessageResponse } from '@/tools/telegram/types'
+import { BlockConfig } from '../types'
+
+export const TelegramBlock: BlockConfig<TelegramMessageResponse> = {
+  type: 'telegram',
+  name: 'Telegram',
+  description: 'Send a message through Telegram',
+  longDescription:
+    'Send messages to any Telegram channel using your Bot API key. Integrate automated notifications and alerts into your workflow to keep your team informed.',
+  category: 'tools',
+  bgColor: '#E0E0E0',
+  icon: TelegramIcon,
+  subBlocks: [
+    {
+      id: 'chatId',
+      title: 'Chat ID',
+      type: 'short-input',
+      layout: 'full',
+      placeholder: 'Enter Telegram Chat ID',
+    },
+    {
+      id: 'text',
+      title: 'Message',
+      type: 'long-input',
+      layout: 'full',
+      placeholder: 'Enter the message to send',
+    },
+    {
+      id: 'botToken',
+      title: 'Bot Token',
+      type: 'short-input',
+      layout: 'full',
+      placeholder: 'Enter your Telegram Bot Token',
+      password: true,
+      connectionDroppable: false,
+    },
+  ],
+  tools: {
+    access: ['telegram_message'],
+  },
+  inputs: {
+    botToken: { type: 'string', required: true },
+    chatId: { type: 'string', required: true },
+    text: { type: 'string', required: true },
+  },
+  outputs: {
+    response: {
+      type: {
+        ok: 'boolean',
+        result: 'json'
+      }
+    }
+  },
+}
