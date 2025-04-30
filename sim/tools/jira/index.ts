3058e195e85ce8db97f80d3b85f83feb70540023
Adam Gough <adamgough@Adams-MacBook-Pro.local>
2025-04-30 15:39:49 -0700

    added jira bulk fetch

--- sim/tools/jira/index.ts
+++ sim/tools/jira/index.ts
@@ -1,7 +1,9 @@
 import { jiraRetrieveTool } from './retrieve'
 import { jiraUpdateTool } from './update'
 import { jiraWriteTool } from './write'
+import { jiraBulkRetrieveTool } from './retrieve_bulk'
 
 export { jiraRetrieveTool }
 export { jiraUpdateTool }
 export { jiraWriteTool }
+export { jiraBulkRetrieveTool }
