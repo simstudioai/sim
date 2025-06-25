b7e4a87697385f4668e71b209c75ab57916883dc
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-06-25 17:16:26 +0200

    feat:updated landing page and models

--- apps/sim/app/(landing)/landing.tsx
+++ apps/sim/app/(landing)/landing.tsx
@@ -3,8 +3,6 @@
 import NavWrapper from './components/nav-wrapper'
 import Footer from './components/sections/footer'
 import Hero from './components/sections/hero'
-import Integrations from './components/sections/integrations'
-import Testimonials from './components/sections/testimonials'
 
 export default function Landing() {
   const handleOpenTypeformLink = () => {
@@ -14,15 +12,7 @@ export default function Landing() {
   return (
     <main className='relative min-h-screen bg-[#0C0C0C] font-geist-sans'>
       <NavWrapper onOpenTypeformLink={handleOpenTypeformLink} />
-
       <Hero />
-      <Testimonials />
-      {/* <Features /> */}
-      <Integrations />
-      {/* <Blogs /> */}
-
-      {/* Footer */}
-      <Footer />
     </main>
   )
 }
