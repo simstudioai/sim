7f4eb2bcc202d969293f3e7fad246bc0bfcc3fee
Vikhyath Mondreti <vikhyath@simstudio.ai>
2025-10-08 18:37:48 -0700

    remove unused type

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,9 +1,8 @@
-import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js'
+import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
 export * from './schema'
-export type { PostgresJsDatabase }
 
 const connectionString = process.env.DATABASE_URL!
 if (!connectionString) {
