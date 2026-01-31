0e7b8430e93dc792f7c82f35a618e64e51386056
waleed <walif6@gmail.com>
2026-01-31 00:51:22 -0800

    feat(tools): added google maps and DSPy

---
+++ apps/sim/tools/dspy/index.ts
@@ -0,0 +1,4 @@
+export { chainOfThoughtTool } from '@/tools/dspy/chain-of-thought'
+export { predictTool } from '@/tools/dspy/predict'
+export { reactTool } from '@/tools/dspy/react'
+export * from '@/tools/dspy/types'
