621dd238a8c0693efab3303fff250f607692d8c5
Siddharth Ganesan <siddharthganesan@gmail.com>
2026-02-09 19:27:17 -0800

    Nuke bad files

--- apps/sim/lib/workflows/blocks/index.ts
+++
@@ -1,7 +0,0 @@
-export { BlockSchemaResolver, blockSchemaResolver } from './schema-resolver'
-export type {
-  ResolvedBlock,
-  ResolvedOption,
-  ResolvedOutput,
-  ResolvedSubBlock,
-} from './schema-types'
