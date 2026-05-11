7ba695e95f87df3ce27e567fd950c3fb41587bdb
Siddharth Ganesan <siddharthganesan@gmail.com>
2026-05-11 15:58:56 -0700

    Fix migs

--- packages/db/migrations/0205_smooth_sentinel.sql
+++ packages/db/migrations/0205_smooth_sentinel.sql
@@ -7,5 +7,6 @@ CREATE TABLE "mothership_settings" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+ALTER TABLE "settings" ADD COLUMN "mothership_environment" text DEFAULT 'default' NOT NULL;--> statement-breakpoint
 ALTER TABLE "mothership_settings" ADD CONSTRAINT "mothership_settings_workspace_id_workspace_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "public"."workspace"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "mothership_settings_workspace_id_idx" ON "mothership_settings" USING btree ("workspace_id");
--- packages/db/migrations/0206_amazing_maximus.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "settings" ALTER COLUMN "mothership_environment" SET DEFAULT 'default';
---> statement-breakpoint
-UPDATE "settings" SET "mothership_environment" = 'default' WHERE "mothership_environment" = 'prod';
