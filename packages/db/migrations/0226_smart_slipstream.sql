fee1cd21233287af459c645b2dacc52637cb75a9
waleed <walif6@gmail.com>
2026-06-05 01:01:02 -0700

    chore(db): drop branch migration 0226 ahead of staging merge; will regenerate

--- packages/db/migrations/0226_smart_slipstream.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE "form" CASCADE;--> statement-breakpoint
-DROP TABLE "template_creators" CASCADE;--> statement-breakpoint
-DROP TABLE "template_stars" CASCADE;--> statement-breakpoint
-DROP TABLE "templates" CASCADE;--> statement-breakpoint
-DROP TYPE "public"."template_creator_type";--> statement-breakpoint
-DROP TYPE "public"."template_status";
