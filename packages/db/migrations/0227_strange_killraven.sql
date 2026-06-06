b0fb041ff0e7b1e3fbd8985281a0832018d201f0
waleed <walif6@gmail.com>
2026-06-06 00:09:46 -0700

    chore(db): regenerate migration as 0227 after staging merge

---
+++ packages/db/migrations/0227_strange_killraven.sql
@@ -0,0 +1,6 @@
+DROP TABLE "form" CASCADE;--> statement-breakpoint
+DROP TABLE "template_creators" CASCADE;--> statement-breakpoint
+DROP TABLE "template_stars" CASCADE;--> statement-breakpoint
+DROP TABLE "templates" CASCADE;--> statement-breakpoint
+DROP TYPE "public"."template_creator_type";--> statement-breakpoint
+DROP TYPE "public"."template_status";
