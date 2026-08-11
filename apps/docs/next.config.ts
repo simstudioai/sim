21fc117a074d3460e99b2bb45a0f641d673a6c4d
Theodore Li <theo@sim.ai>
2026-08-10 18:56:47 -0700

    fix(docs): use TypeScript CLI with Next.js

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -11,6 +11,7 @@ const config: NextConfig = {
     unoptimized: true,
   },
   experimental: {
+    useTypeScriptCli: true,
     webpackMemoryOptimizations: true,
     webpackBuildWorker: true,
   },
